a9fa8e6d04f6bdd764ebea84d0b5362fe101f5b6
admin <admin@MSI>
2016-09-05 22:07:19 -0500

    mensaje

--- JavaApplication10/src/javaapplication10/JavaApplication10.java
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -15,7 +15,7 @@ public class JavaApplication10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        sfgdfxcvzxcvz
+        System.out.println("hola");
         // TODO code application logic here
     }
     
