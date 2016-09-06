068e56a7e6b21612d0ccb436e685481aab8b257f
admin <admin@MSI>
2016-09-05 22:11:29 -0500

    metodo simple

---
+++ JavaApplication10/src/javaapplication10/clase.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author admin
+ */
+public class clase {
+    int numero1=8;
+    int numero2=3;
+    
+    public int sumar(int numero1,int numero2){
+        int resultado=numero1+numero2;
+        return resultado;
+        
+    }
+    
+}
